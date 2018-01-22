8b42180165b3ac941e0b71db02f81d768be3acc4
XiangLei <leixiang5833@163.com>
2018-01-22 12:42:58 +0800

    实现了注册以及登录功能

--- jobplus/forms.py
+++ jobplus/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm  # FlaskForm 为每一个表单(html表单)输入声明一个字段
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import Length, Email, EqualTo, Required  # validators 添加了验证
+from jobplus.models import db, User
+
 
 
 class Company_RegisterForm(FlaskForm):
@@ -11,6 +13,22 @@ class Company_RegisterForm(FlaskForm):
     submit = SubmitField('提交')
     # Field一般提供两个参数，第一个是 html 中的标签，第二个是 validators, 表单提交的时候，会使用验证器进行验证，验证出现错误的将写入 Field 下的 Errors 列表
 
+    def create_user(self):
+        user = User()
+        user.username = self.username.data
+        user.email = self.email.data
+        user.password = self.password.data
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已经存在')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已经存在')
 
 class Seeker_RegisterForm(FlaskForm):
     username = StringField('用户名', validators=[Required(), Length(3,24)])  # 验证长度，即限定长度
@@ -19,10 +37,35 @@ class Seeker_RegisterForm(FlaskForm):
     repeat_password = PasswordField('重复密码', validators=[Required(), EqualTo('password')])
     submit = SubmitField('提交')
 
+    def create_user(self):
+        user = User()
+        user.username = self.username.data
+        user.email = self.email.data
+        user.password = self.password.data
+        db.session.add(user)
+        db.session.commit()
+        return user
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('用户名已经存在')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已经存在')
+
 
 class LoginForm(FlaskForm):
     email = StringField('邮箱', validators=[Required(), Email()])
     password = PasswordField('密码', validators=[Required(), Length(6,24)])
     remember_me = BooleanField('记住我')
     submit = SubmitField('提交')
+    
+    def validate_email(self, field):
+        if field.data and not User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱未注册')
 
+    def validate_password(self, field):
+        user = User.query.filter_by(email=self.email.data).first()
+        if user and not user.check_password(field.data):
+            raise ValidationError('密码错误')
--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,7 +1,9 @@
 # 注册蓝图，将主页路由移入其中
 
-from flask import Blueprint, render_template
+from flask_login import login_user, logout_user, login_required
+from flask import Blueprint, render_template, flash, redirect, url_for
 from jobplus.forms import LoginForm, Seeker_RegisterForm, Company_RegisterForm
+from jobplus.models import User
 
 front = Blueprint('front', __name__)
 
@@ -9,17 +11,37 @@ front = Blueprint('front', __name__)
 def index():
     return render_template('index.html')
 
-@front.route('/login')
+@front.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        login_user(user, form.remember_me.data)  # 第一个参数 User 对象，跌入个参数布尔值，告诉 flask_login 是否要记住该用户
+        return redirect(url_for('.index'))
     return render_template('login.html', form=form)  # form由此传递入 login.html
 
-@front.route('/C-register')
+@front.route('/logout')
+@login_required  # login_required 装饰器保护了这个路由处理函数，未登录状态下访问这个页面会被重定向到登录页面
+def logout():
+    logout_user()
+    flash('您已经退出登录', 'success')
+    return redirect(url_for('.index'))
+
+@front.route('/C-register', methods=['GET', 'POST'])
 def company_register():
     form = Company_RegisterForm()
+    if form.validate_on_submit():
+        form.create_user()
+        flash('注册成功， 请登录！', 'success')
+        return redirect(url_for('.login'))
     return render_template('company_register.html', form=form)
 
-@front.route('/S-register')
+@front.route('/S-register', methods=['GET', 'POST'])
 def seeker_register():
     form = Seeker_RegisterForm()
+    if form.validate_on_submit():  # from.validate.submit是 flask_wtf 提供的封装方法，返回布尔值
+        form.create_user()
+        flash('注册成功， 请登录！', 'success')  # flash() 向模板页面发送一个消息，两个参数，消息内容和分类
+        return redirect(url_for('.login'))
     return render_template('seeker_register.html', form=form)
+
